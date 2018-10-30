b94fd347f78375d74af63b5d207bea229a27baf2
kozakoff <kozakoff@mail.usf.edu>
2018-10-30 16:32:59 -0400

    Removed dead code. Added keypress dispatchEvent and focus/blur for activeElement.

--- Rocket Evaluation/content.js
+++ Rocket Evaluation/content.js
@@ -5,8 +5,41 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) 
 {
 	console.log("Message recieved!");
-	document.activeElement.value = msg.text + document.activeElement.value;
+	
+	//document.activeElement.addEventListener('keyup', (event) => {
+	//	  const keyName = event.key;
+	//	  console.log('keypress event with ' + 'key: ' + keyName);
+	//});
+	
+	document.activeElement.focus();
+	
+	if(document.activeElement.value != "")
+	{
+		document.activeElement.value = msg.text + '\n\n' + document.activeElement.value;
+	}
+	else
+	{
+		document.activeElement.value = msg.text;
+	}
+	
+	var e = new Event("keyup");
+	e.key = "a";
+	e.keyCode = e.key.charCodeAt(0);
+	e.which = e.keyCode;
+	e.altKey = false;
+	e.ctrlKey = false;
+	e.shiftKey = false;
+	e.metaKey = false;
+	e.bubbles = true;
+	document.activeElement.dispatchEvent(e);
+	//document.activeElement.change();
+	//let input = document.getElementById("name");let keyboard = Keysim.Keyboard.US_ENGLISH;let ctrl_shift_enter = new Keysim.Keystroke(  Keysim.Keystroke.CTRL | Keysim.Keystroke.SHIFT,  13);keyboard.dispatchEventsForKeystroke(ctrl_shift_enter, input);
+	
+	document.activeElement.blur();
+
 });
+
+
 
 function sleep(ms) 
 {
@@ -22,26 +55,7 @@ async function init()
 		if(document.querySelector("body"))
 		{
 			console.log("found body");
-			//if(document.querySelector("body > app-root").innerText == 'Loading...')
-			//{
-			//	console.log('Loading...');
-			//	await sleep(1000);	
-			//}
-			//else
-			//{
-			//	loading = false;
-			//	if(document.querySelector("body > app-root > main > app-evaluation-workspace > div > header > div > div > div:nth-child(1) > p:nth-child(3)"))
-			//	{
-			//		var s = document.querySelector("body > app-root > main > app-evaluation-workspace > div > header > div > div > div:nth-child(1) > p:nth-child(3)").innerText;
-			//		chrome.runtime.sendMessage({text: s}, function(response) { console.log("Response: ", response); });
-			//		console.log("content_script says: " + s);
-			//	}
-			//	else
-			//	{
-			//		console.log("The loaded content in body > app-root doesn't look like an evaluation page.");
-			//	}
-			//}
-			
+	
 			loading = false;
 			
 			try
@@ -55,54 +69,34 @@ async function init()
 				console.log("target is not observable. "+e.message);
 			}
 		}
-		//else
-		//{
-		//	loading = false;
-		//}
 	}
 }
 
 function processMutations(mutations) 
 {
-	//mutations.forEach(
-	//	function(mutation)
-	//	{
-	//		console.log(JSON.stringify(mutation.addedNodes));
-			//if(target.innerText == 'Loading...')
-			//{
-				//console.log('Taking a break...');
-				//chrome.runtime.sendMessage({text: 'Taking a break...'}, function(response) { console.log("Response: ", response); });
-				//chrome.runtime.sendMessage({text: 'One second later'}, function(response) { console.log("Response: ", response); });
-				//console.log('One second later');
-			//}
-			//else
-			//{
-				loading = false;
-				
-				//#tbl_maindata > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > div:nth-child(2)
-				
-				if(document.querySelector("body > app-root > main > app-evaluation-workspace > div > header > div > div > div:nth-child(1) > p:nth-child(3)"))
-				{
-					var s = document.querySelector("body > app-root > main > app-evaluation-workspace > div > header > div > div > div:nth-child(1) > p:nth-child(3)").innerText;
-					chrome.runtime.sendMessage({text: s}, function(response) { console.log("Response: ", response); });
-					console.log("content_script says: " + s);
-					//break;
-				}
-				else if(document.querySelector("#tbl_maindata > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > div:nth-child(2)"))
-				{
-					var s = document.querySelector("#tbl_maindata > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > div:nth-child(2)").innerText;
-					chrome.runtime.sendMessage({text: s}, function(response) { console.log("Response: ", response); });
-					console.log("content_script says: " + s);
-				}
-				else
-				{
-					var s = "The loaded content in body doesn't look like an evaluation page: "+target.innerText;
-					//chrome.runtime.sendMessage({text: s}, function(response) { console.log("Response: ", response); });
-					console.log("content_script says: " + s);
-				}	
-			//}
-	//	}
-	//);
+	loading = false;
+			
+	//#tbl_maindata > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > div:nth-child(2)
+			
+	if(document.querySelector("body > app-root > main > app-evaluation-workspace > div > header > div > div > div:nth-child(1) > p:nth-child(3)"))
+	{
+		var s = document.querySelector("body > app-root > main > app-evaluation-workspace > div > header > div > div > div:nth-child(1) > p:nth-child(3)").innerText;
+		chrome.runtime.sendMessage({text: s}, function(response) { console.log("Response: ", response); });
+		console.log("content_script says: " + s);
+		//break;
+	}
+	else if(document.querySelector("#tbl_maindata > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > div:nth-child(2)"))
+	{
+		var s = document.querySelector("#tbl_maindata > tbody > tr:nth-child(1) > td > table > tbody > tr > td:nth-child(1) > div:nth-child(2)").innerText;
+		chrome.runtime.sendMessage({text: s}, function(response) { console.log("Response: ", response); });
+		console.log("content_script says: " + s);
+	}
+	else
+	{
+		var s = "The loaded content in body doesn't look like an evaluation page: "+target.innerText;
+		chrome.runtime.sendMessage({text: s}, function(response) { console.log("Response: ", response); });
+		console.log("content_script says: " + s);
+	}	
 }
 
 try
